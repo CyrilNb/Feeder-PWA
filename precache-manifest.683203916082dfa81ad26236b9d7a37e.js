c4f770dc70dba3bd85a1c63c95f753acce5b7720
Cyril <cyril.niobe@gmail.com>
2018-11-04 15:00:07 +0100

    Updates

---
+++ precache-manifest.683203916082dfa81ad26236b9d7a37e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c743ea947328d639eb79",
+    "url": "/Feeder-PWA/static/js/runtime~main.c743ea94.js"
+  },
+  {
+    "revision": "91cace5259e9750eff27",
+    "url": "/Feeder-PWA/static/js/main.91cace52.chunk.js"
+  },
+  {
+    "revision": "e03e3b54f6a0f7eae10c",
+    "url": "/Feeder-PWA/static/js/1.e03e3b54.chunk.js"
+  },
+  {
+    "revision": "91cace5259e9750eff27",
+    "url": "/Feeder-PWA/static/css/main.7574bdf6.chunk.css"
+  },
+  {
+    "revision": "c32b4773ad0d1d308c82b53d3971115b",
+    "url": "/Feeder-PWA/index.html"
+  }
+];
